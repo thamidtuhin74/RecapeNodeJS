fc95d4124273abf135ebbbd955d31fae92b51531
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-08-18 19:32:03 +0600

    IIFE - Immediately Invoked Function Expression

---
+++ iife.js
@@ -0,0 +1,9 @@
+// Custom iife
+((name, level, designation) => {
+  console.log(`Hi there! I'm here ${name} a ${level} ${designation}.`);
+})("Tahmid Tuhin", "Expert", "Backend Developer");
+
+// Default iife example
+console.log(module);
+console.log(__dirname);
+console.log(__filename);
