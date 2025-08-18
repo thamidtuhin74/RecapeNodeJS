1db64bba026f8dc5bf52b2cfb6d92e04c477e81f
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-08-19 03:13:54 +0600

    Syncronous File System

--- module-2/fileSystem.js
+++ module-2/fileSystem.js
@@ -11,7 +11,25 @@ const newText = "New Text using writeFileSync()";
 fs.writeFileSync('./textfile.txt', newText, {flag: 'a'});
 
 const updateddata = fs.readFileSync('./textfile.txt',{encoding: "utf8"});
-console.log(data);
 console.log(updateddata);
 
 //2. Asyncronous
+console.log("-----------------Asnc OUTPUT----------------")
+
+
+const asncData = "Adding Data unsing AsynReacd Function;"
+fs.writeFile('./textfile.txt', asncData , {flag: 'a'},  (err, asncData)=>{
+    if(err){
+        console.log("Error of File System"+err );
+        return;
+    }
+    console.log("Texxt File Updated: ");
+})
+
+fs.readFile('./textfile.txt',{encoding: 'utf-8'}, (err,data)=>{
+    if(err){
+        console.log("Error of File System"+err );
+        return;
+    }
+    console.log("Data from Async FS readFile(): "+ data); 
+});
