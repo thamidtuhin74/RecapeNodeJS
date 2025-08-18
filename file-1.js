7b88f78f3946836800b2a496ebf31bc07fcc4433
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-08-18 18:08:59 +0600

    Initializinf

---
+++ file-1.js
@@ -0,0 +1,4 @@
+const { a, addFn } = require("./file-2");
+
+console.log(a);
+console.log("sum = " + addFn(56, 45));
---
+++ file-2.js
@@ -0,0 +1,9 @@
+const a = 40;
+
+const addFn = (param1, param2) => {
+  return param1 + param2;
+};
+
+module.exports = { a, addFn };
+
+// console.log(module);
