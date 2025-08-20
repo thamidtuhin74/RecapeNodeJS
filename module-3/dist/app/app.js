77adf17aaa52cf44a1ab390ba098bfac6a1c6ee7
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-08-21 00:02:48 +0600

    express ROuting

--- module-3/dist/app/app.js
+++ module-3/dist/app/app.js
@@ -8,27 +8,48 @@ const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json()); //middlleware & a parser ; Converting JSON to Object
+const todosRouter = express_1.default.Router();
+app.use("/todos", todosRouter);
 const filePath = path_1.default.join(__dirname, "../../db/todo.json");
 const todos = fs_1.default.readFileSync(filePath, { encoding: 'utf-8' });
 app.get('/', (req, res) => {
     res.send("Welcome to ToDo Server");
 });
-app.get('/todos', (req, res) => {
+todosRouter.get('/', (req, res) => {
     console.log(req.query);
     const { title } = req.query;
-    res.json(todos);
+    res.json({
+        "sms": "From Express Router",
+        todos
+    });
     console.log(title);
 });
-app.get('/todos/:title', (req, res) => {
+todosRouter.get('/:title', (req, res) => {
     console.log(req.params);
     // const {title} = req.query;
     res.json(req.params);
 });
-app.post('/todos/create-todo', (req, res) => {
+todosRouter.post('/create-todo', (req, res) => {
     console.log(req.body);
     const { title, body, createdAt } = req.body;
     res.json({ title, body, createdAt });
 });
+// app.get('/todos', (req: Request, res: Response)=>{
+//     console.log(req.query)
+//     const {title} = req.query;
+//     res.json(todos)
+//     console.log(title)
+// })
+// app.get('/todos/:title', (req: Request, res: Response)=>{
+//     console.log(req.params)
+//     // const {title} = req.query;
+//     res.json(req.params)
+// })
+// app.post('/todos/create-todo', (req: Request, res: Response)=>{
+//     console.log(req.body)
+//     const {title, body, createdAt} = req.body;
+//     res.json({title, body, createdAt});
+// })
 // app - router handleing, middleware
 // server - server handleing - StaticRange, stop, 
 exports.default = app;
--- module-3/src/app/app.ts
+++ module-3/src/app/app.ts
@@ -5,31 +5,58 @@ const app: Application = express()
 
 app.use(express.json()); //middlleware & a parser ; Converting JSON to Object
 
+const todosRouter = express.Router();
+app.use("/todos", todosRouter);
+
 const filePath = path.join(__dirname, "../../db/todo.json")
 const todos = fs.readFileSync(filePath, {encoding: 'utf-8'});
+
+
 
 app.get('/', (req: Request, res: Response)=>{
     res.send("Welcome to ToDo Server");
 })
-app.get('/todos', (req: Request, res: Response)=>{
+todosRouter.get('/', (req: Request, res: Response)=>{
     console.log(req.query)
     const {title} = req.query;
-    res.json(todos)
+    res.json({
+        "sms": "From Express Router",
+        todos
+    })
     console.log(title)
 })
-app.get('/todos/:title', (req: Request, res: Response)=>{
+todosRouter.get('/:title', (req: Request, res: Response)=>{
     console.log(req.params)
     // const {title} = req.query;
     
     res.json(req.params)
 })
-app.post('/todos/create-todo', (req: Request, res: Response)=>{
+todosRouter.post('/create-todo', (req: Request, res: Response)=>{
     console.log(req.body)
     const {title, body, createdAt} = req.body;
     
 
     res.json({title, body, createdAt});
 })
+// app.get('/todos', (req: Request, res: Response)=>{
+//     console.log(req.query)
+//     const {title} = req.query;
+//     res.json(todos)
+//     console.log(title)
+// })
+// app.get('/todos/:title', (req: Request, res: Response)=>{
+//     console.log(req.params)
+//     // const {title} = req.query;
+    
+//     res.json(req.params)
+// })
+// app.post('/todos/create-todo', (req: Request, res: Response)=>{
+//     console.log(req.body)
+//     const {title, body, createdAt} = req.body;
+    
+
+//     res.json({title, body, createdAt});
+// })
 
 // app - router handleing, middleware
 // server - server handleing - StaticRange, stop, 
