f363d7cf72ac0dec37607c0b124eb7c21f3dd1b4
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-08-20 13:36:07 +0600

    updated todo server, sending text, json & string response from the server

--- module-2/toDo-app/server.js
+++ module-2/toDo-app/server.js
@@ -1,15 +1,29 @@
 const http = require('http');
 // console.log(http);
 
+
+const toDos = [
+    { id: 1, title: "Learn Node.js", completed: false },
+    { id: 2, title: "Build an API", completed: true },
+    { id: 3, title: "Practice daily", completed: false }
+  ]
 const server = http.createServer((req, res)=>{
-    // console.log(req.url, req.method);
+    console.log(req.url, req.method);
     // res.end("Welcome to ToDo App server");
 
     if(req.url === "/todos" && req.method === "GET"){
-        res.end("Get Al Todos;");
+       res.writeHead(201, {
+        // "content-type" : "text/plain",
+        "content-type" : "application/json",
+        "email": "admin@gmail.com"
+       });
+        res.end(JSON.stringify(toDos));
     }
     else if(req.url === "/todos/create-todo" && req.method === "POST"){
-        res.end("Created a Todo;")
+        res.writeHead(202,{
+            "content-type" : "text/html"
+        })
+        res.end(`<h2>Add a ne ToDo</h2>`)
     }
     else{
         res.end("Route not found;")
